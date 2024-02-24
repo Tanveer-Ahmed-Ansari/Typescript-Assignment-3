b25b8f7be947d0b42fe7925f7c005f7672ebec1b
Tanveer-Ahmed-Ansari <159915506+Tanveer-Ahmed-Ansari@users.noreply.github.com>
2024-02-24 16:43:22 +0500

    Add files via upload

---
+++ assigment 3/Q3.ts
@@ -0,0 +1,8 @@
+// 3. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let firstName: string = "Tanveer ahmed";
+
+
+console.log (firstName.toLowerCase());
+console.log (firstName.toUpperCase());
+console.log (firstName.charAt(0).toUpperCase() + firstName.slice());
